147b9b7384da205d33323216e6f2a7628e54f846
Matti Tahvonen <matti@vaadin.com>
2013-02-03 11:03:42 +0200

    Cleaned up and modified leaflet css to work with GWT client bundle

--- src/gwtl-core/src/main/java/org/discotools/gwt/leaflet/client/LeafletClienBundle.java
+++ src/gwtl-core/src/main/java/org/discotools/gwt/leaflet/client/LeafletClienBundle.java
@@ -15,12 +15,6 @@ public interface LeafletClienBundle extends ClientBundle {
     @CssResource.NotStrict
     CssResource css();
  
-    @Source("images/zoom-in.png")
-    ImageResource zoomIn();
-
-    @Source("images/zoom-out.png")
-    ImageResource zoomOut();
-
     @Source("images/layers.png")
     ImageResource layers();
 
